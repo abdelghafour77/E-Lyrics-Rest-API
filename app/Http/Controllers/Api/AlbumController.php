51ebd47c86c39e256ecb55da8949a4d410cb0a77
faissal17 <faissalaoukacha@gmail.com>
2023-02-20 17:30:04 +0100

    validation store  album

---
+++ app/Http/Controllers/Api/AlbumController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AlbumResource;
+use App\Models\Album;
+use Illuminate\Http\Request;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $album = Album::create([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+        return new AlbumResource($album);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+        return new AlbumResource($album);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Album $album)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Album $album)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        //
+    }
+}
